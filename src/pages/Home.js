68ad090e3a1fe8b6d34c8b6b523920d1896ba0e5
SeraajDV <seraajdevilliers@gmail.com>
2020-08-24 13:45:13 +0200

    Search component styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import '../styles/Home.css';
-import { Link } from 'react-router-dom';
-import AppsIcon from '@material-ui/icons/Apps';
-import { Avatar } from '@material-ui/core';
+import React from "react";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import logo from "../assets/logo.png";
+import Search from '../components/Search';
 
 function Home() {
-    return (
-        <div className="Home">
-            <div className="home__header">
-                <div className="home__headerLeft">
-                    <Link to='/about'>About</Link>
-                    <Link to='/store'>Store</Link>
-                </div>
-                <div className="home__headerRight">
-                <Link to='/gmail'>Gmail</Link>
-                <Link to='/images'>Images</Link>
-                <AppsIcon />
-                <Avatar />
-                </div>
-            </div>
-
-            <div className="home__body">
-
-            </div>
+  return (
+    <div className="Home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
-    )
+        <div className="home__headerRight">
+          <Link to="/mail">Mail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img src={logo} alt="Meggle" />
+        <div className="home__inputContainer">
+            <Search />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
