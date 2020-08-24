b243d7a971dfdc630e7b711aabf448f6c7721ff5
SeraajDV <seraajdevilliers@gmail.com>
2020-08-24 12:09:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Meggle </h1>
+    <div className="app">
+      <h1>Meggle</h1>
+
+      {/* Home */}
+      <Home />
+      {/* SearchPage */}
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <h1>Home page</h1>
+
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    {/* Link */}
+                    {/* Link */}
+                </div>
+                <div className="home__headerRight">
+                    {/* Link */}
+                    {/* Link */}
+                    {/* Link */}
+                    {/* Icon */}
+                    {/* Avatar */}
+                </div>
+            </div>
+
+            <div className="home__body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
