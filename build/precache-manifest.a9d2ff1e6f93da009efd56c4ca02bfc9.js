dda9b46b406229bac9df24b430d7e018d8408393
SeraajDV <seraajdevilliers@gmail.com>
2020-08-25 13:44:52 +0200

    complete

---
+++ build/precache-manifest.a9d2ff1e6f93da009efd56c4ca02bfc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f8b440b85ed8565d6f80e2609722a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2012213a8ff04e0323",
+    "url": "/static/css/main.8f896584.chunk.css"
+  },
+  {
+    "revision": "31d6986049c7069a88e6",
+    "url": "/static/js/2.b275829d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b275829d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2012213a8ff04e0323",
+    "url": "/static/js/main.58a169c4.chunk.js"
+  },
+  {
+    "revision": "97b1df3465406a2b8f4c",
+    "url": "/static/js/runtime-main.30adb086.js"
+  },
+  {
+    "revision": "c9b3f1a88fa4f01bff678463ebc1d7be",
+    "url": "/static/media/logo.c9b3f1a8.png"
+  }
+]);
